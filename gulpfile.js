b2979655bfc0ed807c034077d1a9505894eee9c3
Dave <mariobros1068@hotmail.com>
2021-04-24 17:37:36 -0500

    Creando un watch en el archivo gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -16,8 +16,14 @@ function css() {
         .pipe(gulp.dest('css'));
 }
 
+function watchFiles() {
+    gulp.watch('scss/*.scss', css);
+    gulp.watch('index.html')
+}
+
 
 
 //Registrar funciones como tareas
 
 gulp.task('css', css);
+gulp.task('watch', gulp.parallel(watchFiles));
