f0621fa8a229b26ee3605a4e6a1c8d12549661b5
Dave <mariobros1068@hotmail.com>
2021-04-24 17:07:59 -0500

    Configurando el archivo gulfile.js

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+
+function css() {
+    return gulp
+        .src('scss/app.scss')
+        .pipe(autoprefixer({
+            overrideBrowserslist : ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sass({
+            outputStyle: 'expanded',
+        }))
+        .pipe(gulp.dest('css'));
+}
+
+
+
+//Registrar funciones como tareas
+
+gulp.task('css', css);
